2f3bc97d0a531b5c9b420277ec7dcc556dea6357
shibambanik <shibambanik50@gmail.com>
2025-08-03 00:00:31 +0530

    publisher imports

--- src/main/java/com/chatter/controller/ChatterController.java
+++ src/main/java/com/chatter/controller/ChatterController.java
@@ -2,7 +2,10 @@ package com.chatter.controller;
 
 import com.chatter.service.KafkaProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/messages")
